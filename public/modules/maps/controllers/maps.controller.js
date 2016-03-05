d8dc4f035bffb45bbb7fb48c0153d454d75a2b7b
Ramkumar Dhinakaran <ramkumar.dhinakaran@perficient.com>
2016-03-05 22:38:04 +0530

    WIP

--- public/modules/maps/controllers/maps.controller.js
+++ public/modules/maps/controllers/maps.controller.js
@@ -24,110 +24,123 @@
                            SimpleMarkerSymbol, SimpleLineSymbol, SimpleFillSymbol,
                            geometryEngineAsync, PopupTemplate,PictureMarkerSymbol) {
 
+                 $scope.getCurrentLocation(function(err,position){
+
+                     if(!err) {
+                         console.log(position);
+                         var lat = position ? position.latitude : 80.20000069999999;
+                         var lng = position ? position.longitude : 13.014060599999999;
+
+                         console.log(position.latitude.toFixed(4) + '  ' + position.longitude);
+                         self.latlng = position.latitude.toFixed(4) + ',' + position.longitude.toFixed(4);
+                         self.map = new Map({
+                             basemap: 'streets',
+                             zoom: 15
+                         });
+
+                         //Initial Load
+                         if (!$scope.$$phase) {
+                             $scope.$apply();
+                         }
+                         self.onViewCreated = function (view) {
+                             self.view = view;
+                         };
+                         var graphicsLayer = new GraphicsLayer();
+                         self.map.then(function () {
+                             self.map.add(graphicsLayer);
+                             self.graphicsLayer = graphicsLayer;
+
+                             var pointSym = new PictureMarkerSymbol('/assets/modules/core/img/defaultMarker.png', 40, 40);
+                             self.point = pointSym;
+                             self.sr = new SpatialReference(4326);
+
+                             var pictureMarkerSymbol = new PictureMarkerSymbol('/assets/modules/core/img/current.png', 50, 50);
+
+                             self.loadMarker(lat, lng, pictureMarkerSymbol);
+
+                         });
+
+                         self.loadMarker = function (lat, long, symbol, attribute) {
 
 
-                     if (navigator.geolocation) {
-                         navigator.geolocation.getCurrentPosition(function(position){
-
-                             var lat = position.coords ? position.coords.latitude : 80.20000069999999;
-                             var lng = position.coords ? position.coords.longitude: 13.014060599999999;
-
-                             console.log(position.coords.latitude.toFixed(4)+'  '+position.coords.longitude);
-                             self.latlng = position.coords.latitude.toFixed(4)+','+position.coords.longitude.toFixed(4);
-                             self.map = new Map({
-                                 basemap: 'streets',
-                                 zoom: 15
+                             var point = new Point({
+                                 x: long,
+                                 y: lat,
+                                 spatialReference: self.sr
                              });
 
-                             //Initial Load
-                             if (!$scope.$$phase){
-                                 $scope.$apply();
-                             }
-                             self.onViewCreated = function(view) {
-                                 self.view = view;
+                             var lineAtt = attribute ? attribute : {
+                                 Name: "Your Current Location"
                              };
-                             var graphicsLayer = new GraphicsLayer();
-                             self.map.then( function(){
-                                 self.map.add( graphicsLayer);
-                                 self.graphicsLayer = graphicsLayer;
-
-                                 var pointSym = new PictureMarkerSymbol('/assets/modules/core/img/defaultMarker.png', 40, 40);
-                                 self.point = pointSym;
-                                 self.sr = new SpatialReference(4326);
-
-                                 var pictureMarkerSymbol = new PictureMarkerSymbol('/assets/modules/core/img/current.png', 50, 50);
-
-                                 self.loadMarker(lat,lng,pictureMarkerSymbol);
-
-                             });
-
-                             self.loadMarker = function(lat,long,symbol,attribute){
 
 
-                                 var point = new Point({
-                                     x: long,
-                                     y: lat,
-                                     spatialReference: self.sr
-                                 });
+                             geometryEngineAsync.geodesicBuffer(point, 50, 'yards')
+                                 .then(function (buffer) {
 
-                                 var lineAtt = attribute ? attribute : {
-                                     Name: "Your Current Location"
-                                 };
+                                     self.graphicsLayer.add(new Graphic({
+                                         geometry: point,
+                                         symbol: symbol ? symbol : self.point,
+                                         attributes: lineAtt,
+                                         popupTemplate: new PopupTemplate({
+                                             title: "{Name}",  //The title of the popup will be the name of the pipeline
+                                             content: "{*}"  //Displays a table of all the attributes in the popup
+                                         })
+                                     }));
+                                     return buffer;
+                                 }).then(function (geom) {
 
-
-
-                                 geometryEngineAsync.geodesicBuffer(point, 50, 'yards')
-                                     .then(function(buffer){
-
-                                         self.graphicsLayer.add(new Graphic({
-                                             geometry: point,
-                                             symbol: symbol ? symbol : self.point,
-                                             attributes: lineAtt,
-                                             popupTemplate: new PopupTemplate({
-                                                 title: "{Name}",  //The title of the popup will be the name of the pipeline
-                                                 content: "{*}"  //Displays a table of all the attributes in the popup
-                                             })
-                                         }));
-                                         return buffer;
-                                     }).then(function(geom){
-
-                                     // Zoom to newly added point
-                                     return self.view.then(function() {
-                                         // animate to the buffer geometry
-                                         return self.view.animateTo(geom).then(function() {
-                                             // when the animation completes, set the scale to 1:24,000
-                                             self.view.scale = 24000;
-                                             // resolve the promises with the input geometry
-                                             return geom;
-                                         });
+                                 // Zoom to newly added point
+                                 return self.view.then(function () {
+                                     // animate to the buffer geometry
+                                     return self.view.animateTo(geom).then(function () {
+                                         // when the animation completes, set the scale to 1:24,000
+                                         self.view.scale = 24000;
+                                         // resolve the promises with the input geometry
+                                         return geom;
                                      });
                                  });
-
-
-                                 /*var gra = new Graphic(myPoint);
-                                  self.graphicsLayer.add(gra);*/
-                             };
-
-                         }, function(msg){
-                             console.log('not supported: '+ msg);
-                         });
-                     } else {
-                         console.log('not supported');
+                             });
+                         };
                      }
-
-
-
-
+                 });
              });
 
-            $scope.getCurrentLocation = function(){
+            $scope.getCurrentLocation = function(callBack){
                 if(!navigator.geolocation)
                 {
-                    return null;
+                    callBack('Error  - Broswer Doesn\'t Support HTML5 Geolocation ',null);
                 }
                 else if(navigator.geolocation) {
                     navigator.geolocation.getCurrentPosition(function (position) {
-                        return position.coords;
+                        console.log('Retrieved position using HTML5 Geolocations');
+                        callBack(null,position.coords);
+                    },function(err){
+                        $http({
+                            method: 'GET'
+                            ,url:'//freegeoip.net/json/'
+                        }).then(function(location) {
+                            console.log('Retrieved position using Geoip');
+                            location = location.data;
+                            var position ={ coords : {
+                                latitude: location.latitude ,longitude: location.longitude
+                            } };
+                            callBack(null,position.coords);
+                        });
+
+                        /*$.ajax( {
+                            url: '//freegeoip.net/json/',
+                            type: 'POST',
+                            dataType: 'jsonp',
+                            success: function(location) {
+
+                                var position ={ coords : {
+                                    latitude: location.latitude ,longitude: location.longitude
+                                } };
+
+                                console.log('Retrieved position using Geoip');
+                                callBack(null,position.coords);
+                            }
+                        } );*/
                     });
                 }
             };
